7abfe836ddf7b77a60ef55630018260956411a6a
Rejan Bajracharya <buzz.rejan@gmail.com>
2021-02-16 17:12:19 +0545

    id generator

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,22 +1,24 @@
 import React from "react";
 import ShoppingListForm from "./ShoppingListForm";
+import { v4 as uuid } from "uuid";
 export default class ShoppingList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [
-        { name: "milk", qty: 2 },
-        { name: "banana", qty: 3 },
+        { name: "milk", qty: 2, id: uuid() },
+        { name: "banana", qty: 3, id: uuid() },
       ],
     };
     this.addItem = this.addItem.bind(this);
   }
   addItem(item) {
-    this.setState((st) => ({ items: [...st.items, item] }));
+    let newItem = { ...item, id: uuid() };
+    this.setState((st) => ({ items: [...st.items, newItem] }));
   }
   renderList() {
     let list = this.state.items.map((item) => (
-      <li>
+      <li key={item.id}>
         name:{item.name} qty: {item.qty}
       </li>
     ));
